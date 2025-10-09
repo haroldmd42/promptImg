c21a30cbf816df3531de710e31d47470f2755925
haroldmd42 <yanmd4266@gmail.com>
2025-10-09 16:40:24 -0500

    Deplyment

--- prompt-img/src/App.jsx
+++ prompt-img/src/App.jsx
@@ -3,7 +3,9 @@ import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import './App.css'
 import Navbar from './components/navbar/NavBar'
 import Landing from './components/landing/Landing'
-import Collections from './components/collections'
+import Collections from './components/Collections'
+
+
 
 function App() {
 
