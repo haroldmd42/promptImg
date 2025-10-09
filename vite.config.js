1b741a13365f0ba7124bd19738f62e19153ae36d
haroldmd42 <yanmd4266@gmail.com>
2025-10-09 17:03:29 -0500

    update

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { fileURLToPath } from 'url'
-import { dirname, resolve } from 'path'
 
-// 🔧 recrea __dirname de forma compatible con ES Modules
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-
+// 👇 BASE debe coincidir con el nombre EXACTO de tu repo GitHub
 export default defineConfig({
   plugins: [react()],
-  base: '/promptImg/',
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-    },
-  },
+  base: '/promptImg/', // 👈 muy importante
 })
