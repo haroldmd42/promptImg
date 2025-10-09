32dcfdadfe817842429d58c33cb7d76e6da20a63
haroldmd42 <yanmd4266@gmail.com>
2025-10-09 16:07:53 -0500

    Update routes

--- prompt-img/vite.config.js
+++ prompt-img/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// 👇 Agrega el "base" con el nombre exacto de tu repo
 export default defineConfig({
   plugins: [react()],
+  base: '/promptImg/', // 👈 muy importante
 })
