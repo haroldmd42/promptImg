184658c96c01411d9ad6b7e0f79f5e2fd236e8ee
haroldmd42 <yanmd4266@gmail.com>
2025-10-14 19:16:53 -0500

    UPDATE: Navbar

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,28 +1,47 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./NavBar.css";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className="navbar-futuristic">
       <div className="navbar-inner">
+        {/* Logo */}
         <NavLink className="navbar-logo" to="/">
-          <img src={`${import.meta.env.BASE_URL}logo.png`}alt="Logo" />
-          
+          <img
+            src={`${import.meta.env.BASE_URL}logo.png`}
+            alt="Logo"
+            className="navbar-logo-img"
+          />
         </NavLink>
 
-        <ul className="navbar-links">
+        {/* Botón hamburguesa */}
+        <button
+          className={`navbar-toggle ${isOpen ? "open" : ""}`}
+          onClick={() => setIsOpen(!isOpen)}
+          aria-label="Toggle navigation"
+        >
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </button>
+
+        {/* Links */}
+        <ul className={`navbar-links ${isOpen ? "show" : ""}`}>
           <li>
-            <NavLink to="/" end className="nav-link">
+            <NavLink to="/" end className="nav-link" onClick={() => setIsOpen(false)}>
               Inicio
             </NavLink>
           </li>
           <li>
-            <NavLink to="/collections" className="nav-link">
+            <NavLink to="/collections" className="nav-link" onClick={() => setIsOpen(false)}>
               Colecciones
             </NavLink>
           </li>
           <li>
-            <NavLink to="/how-to-use" className="nav-link">
+            <NavLink to="/how-to-use" className="nav-link" onClick={() => setIsOpen(false)}>
               Cómo usar
             </NavLink>
           </li>
